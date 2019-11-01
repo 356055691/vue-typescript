5b686b9db2343449e7671223e14a91c016b6d2f2
李靖 <356055691@qq.com>
2019-11-01 09:31:53 +0800

    demo

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,113 +1,54 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+    <h1>{{ msg }}--{{ names }}</h1>
+    <input type="text" v-model="txt">
+    <p>{{ getTxt }}</p>
+    <button @click="add">add</button>
+    <p>{{ sum }}</p>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
+<script lang="ts">
+import { Component, Prop, Vue, Watch } from 'vue-property-decorator';
+
+@Component
+export default class HelloWorld extends Vue {
+  //props
+  @Prop() private msg!: string
+  @Prop() private names!: string
+  //data
+  private txt: string = '1'
+  private sum: number = 0
+  //computed
+  get getTxt(){
+    return this.txt
   }
+  //methods
+  private add(){
+    this.sum++
+    console.log(`sum : ${this.sum}`)
+  }
+  //生命周期
+  created(){
+    console.log('created')
+  }
+  //watch
+  @Watch('txt') 
+  changeTxt(newTxt: string, oldTxt: string){
+    console.log(`change txt: ${oldTxt} to ${newTxt}`)
+  }
+  
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
+h3 {
+  margin: 40px 0 0;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+input {
+  width: 240px;
+  height: 32px;
+  line-height: 32px;
 }
 </style>
